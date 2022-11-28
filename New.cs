e65206f17692c95de7d4784f4291742ad8194c12
NopphasinSriburin <119285223+NopphasinSriburin@users.noreply.github.com>
2022-11-28 17:04:28 +0700

    F

---
+++ New.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Data tye
+using LABOOP00A;
+
+Student Student1 = new Student();
+Student1.name = "Pu";
+Student1.weight = 69f;
+
+Student Student2 = new Student();
+Student2.name = "Net";
+Student2.weight = 56f;
+
+Student Student3 = new Student();
+Student3.name = "Big";
+Student3.weight = 59f;
+
+Student Student4 = new Student();
+Student4.name = "Focus";
+Student4.weight = 60f;
+
+Student Student5 = new Student();
+Student5.name = "Not";
+Student5.weight = 63f;
+
+
+
+
+
+
