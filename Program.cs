0ac80b8995af0497d53a6973f174c546a8cdeac3
NopphasinSriburin <119285223+NopphasinSriburin@users.noreply.github.com>
2022-11-28 15:53:57 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("นภสินธุ์ ศรีบุรินทร์");
+Console.WriteLine("653450290-8");
+Console.WriteLine("Nopphasin.s@kkumail.com");
