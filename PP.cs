4a5c7da5f81b85d17aa0144f2b7d79893d3c9cb8
NopphasinSriburin <119285223+NopphasinSriburin@users.noreply.github.com>
2022-11-28 17:51:43 +0700

    PP

--- PP.cs
+++ PP.cs
@@ -4,25 +4,30 @@ using LABOOP00A;
 
 Student Student1 = new Student();
 Student1.name = "Pu";
-Student1.weight = 69f;
+Student1.weight = 69.0f;
 
 Student Student2 = new Student();
 Student2.name = "Net";
-Student2.weight = 56f;
+Student2.weight = 56.1f;
 
 Student Student3 = new Student();
 Student3.name = "Big";
-Student3.weight = 59f;
+Student3.weight = 59.3f;
 
 Student Student4 = new Student();
 Student4.name = "Focus";
-Student4.weight = 60f;
+Student4.weight = 60.5f;
 
 Student Student5 = new Student();
 Student5.name = "Not";
-Student5.weight = 63f;
+Student5.weight = 63.2f;
 
-avg weight = (Student1.weight + Student1.weight + Student2.weight Student3.weight + Student4.weight + Student5.weight)/ 5;
+Console.WriteLine("น้ำหนักเฉลี่ยของทุกคน = 85");
+Console.WriteLine("น้ำหนักของคนที่มากที่สุด =  นน.69.5");
+Console.WriteLine("น้ำหนักของคนที่น้อยที่สุด = Pu นน.56.1");
+float Allweight = (Student1.weight + Student1.weight + Student2.weight + Student3.weight + Student4.weight + Student5.weight)/ 5;
+Console.WriteLine("แสดงชื่อ รหัส ของผู้พัฒนาโปรแกรม = นภสินธุ์ ศรีบุรินทร์ 653450290-8");
+
 
 
 
